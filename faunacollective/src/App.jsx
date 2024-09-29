257f9debe82ee671bd760ddf8b5c68af161b6f9b
Pinamelonade <150365241+Pinamelonade@users.noreply.github.com>
2024-09-28 23:22:37 -0400

    flashcard functions

--- faunacollective/src/App.jsx
+++ faunacollective/src/App.jsx
@@ -2,15 +2,39 @@ import './App.css';
 import Flashcard from './components/Flashcard';
 
 const App = () => {
+  const flashcardData = [
+    {id: 1, question: 'What is a group of lions called?', answer: 'A pride'},
+    {id: 2, question: 'What is a group of fish called?', answer: 'A school'},
+    {id: 3, question: 'What is a group of crows called?', answer: 'A murder'},
+    {id: 4, question: 'What is a group of flamingos called?', answer: 'A flamboyance'},
+    {id: 5, question: 'What is a group of ants called?', answer: 'A colony'} 
+  ];
+
+  //arrow buttons
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const goPrev = () => {
+    setCurrentIndex(currentIndex === 0 ? flashcardData.length - 1 : currentIndex - 1 );
+  } 
+
+  const goNext = () => {
+    setCurrentIndex(currentIndex === flashcardData.length - 1 ?  0 : currentIndex + 1 );
+  }
 
   return (
     <div className="App">
       <h1> Fauna Collective </h1>
       <h3> The noun for a group of animal changes depending on the animal! How many do you know?</h3>
+      
       <div className="card-container">
-        <Flashcard />
+        <Flashcard question = {flashcardData[currentIndex].question} answer={flashcardData[currentIndex].answer} />
       </div>
-
+      <div className = "arrows">
+        <span>
+            <button onClick = {goPrev}> ← </button>
+            <button onClick = {goNext}> → </button>
+        </span>
+        </div>
     </div>
   )
 }
--- faunacollective/src/components/Flashcard.jsx
+++ faunacollective/src/components/Flashcard.jsx
@@ -1,43 +1,18 @@
 import React from 'react';
 const Flashcard =(props)=>{
     //flip card
-    const [flip, setFlip] = React.useState(false); 
+    const [flipped, setFlip] = React.useState(false); 
     const upDateFlip = () => {
-        setFlip(!flip);
-    }
-    //arrow buttons
-    const [propsId, setPropsId] = React.useState(0);
-    const goLeft = () => {
-        if(propsId === 0){
-            setPropsId(props.data.length - 1);
-        }else{
-            setPropsId(propsId - 1);
-        }
+        setFlip(!flipped);
     } 
-
-    const goRight = () => {
-        if(propsId === props.data.length - 1){
-            setPropsId(0);
-        }else{
-            setPropsId(propsId + 1);
-        }
-    }  
-
+    
     return(
-        <div className = {'Card'} onClick = {upDateFlip}>
-            <div className = "front"> 
-                <h5> {props.question} </h5>
+    <div className = {"Flashcard"}>
+            <div className = 'card-content' onClick = {upDateFlip} > 
+                <h5> {flipped? props.answer : props.question} </h5> //if flipped is true, show answer, else show question
             </div>
-            <div className = "back"> 
-                <h5> {props.answer} </h5>
-            </div>
-            <div className = "arrows">
-                <span>
-                    <button onClick = {goLeft}> ← </button>
-                    <button onClick = {goRight}> → </button>
-                </span>
-            </div>
-        </div>
+    </div>
     )
 }
+
 export default Flashcard;
